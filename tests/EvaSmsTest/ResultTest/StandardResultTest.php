c92743876b8b6aa4293740f10483552721d0148f
AlloVince <allo.vince@gmail.com>
2015-02-09 19:34:23 +0800

    add more tests

---
+++ tests/EvaSmsTest/ResultTest/StandardResultTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Eva\EvaSmsTest\ResultTest;
+
+
+use Eva\EvaSms\Message\StandardMessage;
+use Eva\EvaSms\Result\StandardResult;
+use Guzzle\Http\Message\Response;
+
+class StandardResultTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+    }
+
+    public function testResult()
+    {
+        $result = new StandardResult(new StandardMessage('+8615212345678', 'MessageBody'), new Response(200));
+        $this->assertTrue(is_numeric($result->getSentTimestamp()));
+    }
+}
+
